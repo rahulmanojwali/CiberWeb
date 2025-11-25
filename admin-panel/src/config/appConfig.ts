c1873a027dc5343a02b26e6f557d406428a6ea00
Rahul Wali <rahulwali@gmail.com>
2025-11-25 10:28:50 +0530

    Sync changes

--- admin-panel/src/config/appConfig.ts
+++ admin-panel/src/config/appConfig.ts
@@ -1,4 +1,6 @@
-export const API_BASE_URL = "/api";
+// export const API_BASE_URL = "/api";
+export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "/api";
+
 
 export const API_TAGS = {
   ORGS: {

79da7e39ab7a649356624568de1ec3b12f16b908
Rahul Wali <rahulwali@gmail.com>
2025-12-30 19:24:34 +0530

    fixing compile bug

--- admin-panel/src/services/setupStepupInterceptor.ts
+++ admin-panel/src/services/setupStepupInterceptor.ts
@@ -1,15 +1,13 @@
-import axios, { type AxiosRequestHeaders } from "axios";
+import axios, { AxiosHeaders } from "axios";
 
 axios.interceptors.request.use(
   (config) => {
     if (typeof window !== "undefined") {
       const stepupSessionId = localStorage.getItem("cm_stepup_session_id");
       if (stepupSessionId) {
-        const currentHeaders = (config.headers || {}) as AxiosRequestHeaders;
-        config.headers = {
-          ...currentHeaders,
-          "X-StepUp-Session": stepupSessionId,
-        };
+        const currentHeaders = AxiosHeaders.from(config.headers || {});
+        currentHeaders.set("X-StepUp-Session", stepupSessionId);
+        config.headers = currentHeaders;
       }
     }
     return config;
